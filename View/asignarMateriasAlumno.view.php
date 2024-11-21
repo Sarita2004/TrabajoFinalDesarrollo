2f3c7b48f02de94468b1af8a9ccdfb3c16dc0f5d
SARITA <sarameliti3@gmail.com>
2024-11-21 18:57:24 -0300

    Final Desarrolloo

--- View/asignarMateriasAlumno.view.php
+++ View/asignarMateriasAlumno.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Materias del Alumno</title>
+    <title>Asignar Materias</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
--- View/indexProfesor.view.php
+++ View/indexProfesor.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Profesores</title>
+    <title>Profesores</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" />
     <script src="//code.jquery.com/jquery-3.5.1.js"></script>
